79ecf0d3e7aa73f28a0bfa882e733ba71a238c3d
rlin <2339219978@qq.com>
2022-08-23 16:48:03 +0800

    项目_06_封装请求库

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-    <div></div>
+    <div>
+        <el-button>123</el-button>
+    </div>
 </template>
 
 <script>
+import { registerAPI } from '@/api'
 export default {
-
+  async created () {
+    const res = await registerAPI()
+    console.log(res)
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/global.less'
-
+import '@/elementUI/index' // 让elementUI注册参与打包，才能在页面上运行并注册插件
 Vue.config.productionTip = false
 
 new Vue({
