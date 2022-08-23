280127071497f4c14e40ee64c6e84d6e41e5b3e9
rlin <2339219978@qq.com>
2022-08-23 17:35:07 +0800

    项目_08_注册和登录路由准备

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,17 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/reg',
+    component: () => import('@/views/register')
+    // webpack 提供import函数来路由懒加载导入组件
+    // 路由懒加载，就是页面路由路径切换到/reg，才去加载对应组件代码
+    // 好处：让首页加载文件体积更新，打开更快
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/register/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Reg.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-register'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
